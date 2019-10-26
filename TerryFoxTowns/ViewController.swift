2bec92eb2326a6a39805e9f16c1059a06c035e0a
Russell Gordon <github@russellgordon.ca>
2019-10-26 16:14:01 -0400

    Working on adding new scaffolding

--- TerryFoxTowns/ViewController.swift
+++ TerryFoxTowns/ViewController.swift
@@ -29,46 +29,47 @@ class ViewController: UIViewController {
         resetLabel()
         
         // Add a few lines of text
-        // NOTE TO STUDENTS: You can delete this section, lines 27 to 57. The code is here only to illustrate what you need to do to get text into the label.
-        appendToLabel(this: "Example town 1")
-        appendToLabel(this: "Example town 2")
-        appendToLabel(this: "Example town 3")
-        appendToLabel(this: "Example town 4")
-        appendToLabel(this: "Example town 5")
-        appendToLabel(this: "Example town 6")
-        appendToLabel(this: "Example town 7")
-        appendToLabel(this: "Example town 8")
-        appendToLabel(this: "Example town 9")
-        appendToLabel(this: "Example town 10")
-        appendToLabel(this: "Example town 11")
-        appendToLabel(this: "Example town 12")
-        appendToLabel(this: "Example town 13")
-        appendToLabel(this: "Example town 14")
-        appendToLabel(this: "Example town 15")
-        appendToLabel(this: "Example town 16")
-        appendToLabel(this: "Example town 17")
-        appendToLabel(this: "Example town 18")
-        appendToLabel(this: "Example town 19")
-        appendToLabel(this: "Example town 20")
-        appendToLabel(this: "Example town 21")
-        appendToLabel(this: "Example town 22")
-        appendToLabel(this: "Example town 23")
-        appendToLabel(this: "Example town 24")
-        appendToLabel(this: "Example town 25")
-        appendToLabel(this: "Example town 26")
-        appendToLabel(this: "Example town 27")
-        appendToLabel(this: "Example town 28")
-        appendToLabel(this: "Example town 29")
+        // NOTE TO STUDENTS: You can delete this section, lines 31 to 61. The code is here only to illustrate what you need to do to get text into the label.
+        prependToLabel(this: "Example town 29")
+        prependToLabel(this: "Example town 28")
+        prependToLabel(this: "Example town 27")
+//        prependToLabel(this: "Example town 26")
+//        prependToLabel(this: "Example town 25")
+//        prependToLabel(this: "Example town 24")
+//        prependToLabel(this: "Example town 23")
+//        prependToLabel(this: "Example town 22")
+//        prependToLabel(this: "Example town 21")
+//        prependToLabel(this: "Example town 20")
+//        prependToLabel(this: "Example town 19")
+//        prependToLabel(this: "Example town 18")
+//        prependToLabel(this: "Example town 17")
+//        prependToLabel(this: "Example town 16")
+//        prependToLabel(this: "Example town 15")
+//        prependToLabel(this: "Example town 14")
+//        prependToLabel(this: "Example town 13")
+//        prependToLabel(this: "Example town 12")
+//        prependToLabel(this: "Example town 11")
+//        prependToLabel(this: "Example town 10")
+//        prependToLabel(this: "Example town 9")
+//        prependToLabel(this: "Example town 8")
+//        prependToLabel(this: "Example town 7")
+//        prependToLabel(this: "Example town 6")
+//        prependToLabel(this: "Example town 5")
+//        prependToLabel(this: "Example town 4")
+//        prependToLabel(this: "Example town 3")
+//        prependToLabel(this: "Example town 2")
+//        prependToLabel(this: "Example town 1")
+        
     }
     
     // Add to the content of the label
     // Use this function to add a new city to the label
     // NOTE TO STUDENTS: Do not delete this function.
-    func appendToLabel(this newText: String) {
+    func prependToLabel(this newText: String) {
         
         // 1. Move to next line in the label (\n)
         // 2. Add the new text (newText)
-        labelOutput.text! += "\n" + newText
+        labelOutput.text! += newText + "\n" + labelOutput.text!
         
     }
     
@@ -95,16 +96,27 @@ class ViewController: UIViewController {
         // 1. width same as it already was
         // 2. height same as label
         //    (recall label was just resized to fit new content)
-        scrollView.contentSize = CGSize(width: scrollView.contentSize.width
-            , height: labelOutput.frame.size.height)
+        scrollView.contentSize = CGSize(width: scrollView.contentSize.width, height: labelOutput.frame.size.height)
 
     }
 
     // NOTE TO STUDENTS
-    // After these comments, you should add an action connected to the button "Show Towns Terry Has Been To".
+    // After these comments, you should fill in the action below that has been connected to the button "Show Towns Terry Has Been To".
     // Then, use the action to obtain the km Terry has run (from the text field) and write logic inside the action to add the towns.
     // Remember, look at the hint given – be sure you review this page:
     // https://docs.swift.org/swift-book/LanguageGuide/ControlFlow.html#ID140
+    
+    @IBAction func showTowns(_ sender: Any) {
+        
+        // Dismisses the keyboard (so it does not block the view of the output)
+        view.endEditing(true)
+
+        // NOTE TO STUDENTS
+        // Add your code below...
+        
+    }
+    
+    
     
 
 }
