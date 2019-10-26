fee0c61c405dfc9019082404020c88fdfc7aff22
Russell Gordon <rgordon@lcs.on.ca>
2019-10-26 08:29:23 -0400

    Initial Commit

---
+++ TerryFoxTowns/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TerryFoxTowns
+//
+//  Created by Russell Gordon on 2019-10-26.
+//  Copyright © 2019 Russell Gordon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
